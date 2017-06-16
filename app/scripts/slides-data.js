7a6acdf3f24fde5d468188c8837c56272d8a825e
Pablo H. Paladino <paladinop@gmail.com>
2017-06-16 13:51:49 -0400

    nuevas historias

--- app/scripts/slides-data.js
+++ app/scripts/slides-data.js
@@ -7,10 +7,9 @@ window.SLIDES_DATA = [
 			{
 				title: 'Buenos Aires detrás de escena',
 				description: 'Millones de historias dan vida a nuestra Ciudad. Algunos disfrutan pasar horas bajo la sombra de un árbol en la plaza del barrio. Otros no frenan nunca: colectivos, subtes y trenes los llevan y traen de todas sus actividades diarias. ¿Qué es lo que más necesitás de tu Ciudad?',
-				type: 'image',
-				url:'http://i2.wp.com/www.buenasdicas.com/wp-content/uploads/2015/11/buenos-aires-monumentos.jpg'
+				type:'image', url:'http://cdn2.buenosaires.gob.ar/desarrollourbano/obrasfinales/Finales%20para%20las%20historias/1.%20SUBE_animacion.gif'
 			}	
-		]
+        ]
 	},
 	{
 		title: 'El microcentro',
@@ -20,95 +19,118 @@ window.SLIDES_DATA = [
 			{
 				title: 'El microcentro',
 				description: 'Para los que trabajan o hacen trámites en el microcentro, caminar era casi imposible. ¿Quién podía atravesar las veredas angostas sin esforzarse para esquivar autos y motos?',
-				type: 'image',
-				url:'images/historias/test/base.png',
-				variations: [
-					{
-						title: 'Reciclaje',
-						url: 'images/historias/test/reciclado.png'
-					},
-					{
-						title: 'Ríos',
-						url: 'images/historias/test/rios.png'
-					}
-				]
-			},
+				type: 'image', url:'http://cdn2.buenosaires.gob.ar/desarrollourbano/obrasfinales/Finales%20para%20las%20historias/2.%20Florida%20Paula%20Soler%20Moya.jpg',
+            },
 			{
 				title: '',
 				description: 'Esta zona tenía lo que se conoce como un índice de compacidad alto: demasiado espacio construido en relación al poco espacio público disponible. Los puntos más  oscuros del mapa representan las zonas que cuentan con menos espacio público, y por lo tanto el índice de compacidad es alto.',
-				type: 'map',
-				url:'http://deluxeargentina.com/wp-content/themes/deluxe/timthumb.php?w=1000&h=450&src=http://deluxeargentina.com/wp-content/uploads/2012/05/Buenos-Aires-by-night.resized.jpg'
-			}
-			,
+				type: 'image', url:'http://cdn2.buenosaires.gob.ar/desarrollourbano/obrasfinales/Finales%20para%20las%20historias/3.%20Compacidad_pre_final.jpg'
+			},
 			{
 				title: '',
 				description: 'Por eso transformamos el microcentro en un área de prioridad peatonal.',
-				type: 'map',
-				url:'http://i1.wp.com/www.buenasdicas.com/wp-content/uploads/2015/11/buenos-aires-capa.jpg'
-			}
-			,
+				type: 'image',	url:'http://www.buenosaires.gob.ar/sites/gcaba/files/img_4675_retocada_2_1.jpg'
+			},
 			{
 				title: '',
-				description: 'Miremos el mapa del indicador de compacidad corregida de nuevo: los puntos que vimos antes ya no son tan oscuros. Eso es porque hoy hay más espacio para caminar tranquilos entre tanto edificio, hay más espacio para disfrutar.',
-				type: 'map',
-				url:'http://argentinafreetravel.com/imgs/ban3.jpg'
+				description: 'Miremos el mapa del indicador de compacidad corregida de nuevo: los puntos que vimos antes ya no son tan oscuros. Eso es porque hoy hay más espacio para caminar tranquilos entre tanto edificio. Hay más espacio para disfrutar. Pero todavía nos falta. Conocé más sobre las obras que estamos realizando en la calle Tacuarí.',
+				type: 'image',	url:'http://cdn2.buenosaires.gob.ar/desarrollourbano/obrasfinales/Finales%20para%20las%20historias/5.%20Compacidad_final.jpg'
 			}
 		]
 	},
 	{
-		title: 'El microcentro II',
+		title: 'Caminar por la Ciudad',
 		order: 2,
+		category: 'integrada',
+		slides: [
+			{
+				title: 'Caminar por la Ciudad',
+				description: 'Pero hacer espacio para que podamos andar a pie por las calles porteñas no es lo único que hace a una verdadera ciudad caminable. También importa que todo lo que te hace falta esté cerca tuyo. Una ciudad integrada es esa donde no necesitás viajar mucho para realizar tus actividades cotidianas. Si tu trabajo, los servicios y comercios que necesitás están al alcance de tu mano, entonces tenés más tiempo para vos.',
+				type: 'image', url:'http://cdn2.buenosaires.gob.ar/desarrollourbano/obrasfinales/Finales%20para%20las%20historias/6.%20Caminabilidad_ninios.jpg',
+			},
+			{
+				title: '',
+				description: 'La Comuna 8 es una de las zonas de la Ciudad con un índice de caminabilidad muy bajo, casi en cero, porque no tiene un buen equilibrio entre la actividad económica, comercial, cultural y/o recreativa. Algunas cuadras superan ampliamente la longitud promedio y superan a los extenuantes 1000 metros, otras incluso se cortan por las vías del ferrocarril o las autopistas. Se las conoce como barreras urbanas, que hacen que sea difícil moverse caminando en esa zona. Los puntos azules del mapa son los que peor resultado dan.',
+				type: 'image', url:'http://cdn2.buenosaires.gob.ar/desarrollourbano/obrasfinales/Finales%20para%20las%20historias/6.%20Caminabilidad_final.jpg'
+			},
+			{
+				title: '',
+				description: 'La construcción de la Villa Olímpica en esta zona es una oportunidad para mejorar en este aspecto: implica construir viviendas, abrir nuevas calles que reduzcan la longitud de las cuadras y sumar comercios y otras actividades económicas y recreativas.',
+				type: 'image',
+				url:'http://www.buenosaires.gob.ar/sites/gcaba/files/styles/interna_pagina/public/villa_olimpica_05.jpg?itok=b_CiZ26W'
+			}
+			
+		]
+	},
+	{
+		title: 'Un sur joven',
+		order: 3,
+		category: 'atractiva',
+		slides: [
+			{
+				title: 'Un sur joven',
+				description: 'La Comuna 8 es, además, una de las zonas de la Ciudad con mayor cantidad de jóvenes. El 35% de sus habitantes tiene menos de 19 años y casi el 50% menos de 40.',
+				type: 'image', url:'http://cdn2.buenosaires.gob.ar/desarrollourbano/obrasfinales/Finales%20para%20las%20historias/7.%20Poblacion.jpg'
+			},
+            {
+				title: '',
+				description: 'Por eso es importante no solo impulsar el desarrollo de viviendas, sino también incentivar la actividad económica, el empleo y las actividades deportivas. La creación del Distrito del Deporte en esta zona busca lograr ese objetivo a través de beneficios fiscales para atraer empresas que se radiquen en la zona.',
+				type: 'image', url:'http://cdn2.buenosaires.gob.ar/desarrollourbano/obrasfinales/Finales%20para%20las%20historias/7.%20Deporte_2.jpg'
+			}	
+		]
+	},
+    {
+		title: 'El equilibrio de la Ciudad',
+		order: 4,
 		category: 'equilibrada',
 		slides: [
 			{
-				title: 'El microcentro II',
-				description: 'Para los que trabajan o hacen trámites en el microcentro, caminar era casi imposible. ¿Quién podía atravesar las veredas angostas sin esforzarse para esquivar autos y motos?',
-				type: 'image',
-				url:'images/historias/test/base.png',
-				variations: [
-					{
-						title: 'Reciclaje',
-						url: 'images/historias/test/reciclado.png'
-					},
-					{
-						title: 'Ríos',
-						url: 'images/historias/test/rios.png'
-					}
-				]
+				title: 'El equilibrio de la Ciudad',
+				description: 'Cuanto más diversas son las actividades de una zona, mayor es su autonomía y su posibilidad de desarrollo. La diversidad nos permite construir una Ciudad más equilibrada.',
+				type: 'image', url:'http://cdn2.buenosaires.gob.ar/desarrollourbano/obrasfinales/Finales%20para%20las%20historias/8.%20disfrute_market.jpg'
+			},	
+            {
+				title: '',
+				description: 'El indicador de equilibrio de usos mide la relación entre el uso residencial y no residencial del suelo: nos muestra dónde falta desarrollo y qué aspecto debemos fortalecer. Si miramos los barrios Urquiza, Coghlan y Saavedra encontramos muchos puntos de color rojo. Esto muestra que el indicador está muy bajo, porque es una zona que siempre fue residencial, de casas bajas y calles amplias.',
+				type: 'image', url:'http://cdn2.buenosaires.gob.ar/desarrollourbano/obrasfinales/Finales%20para%20las%20historias/7.%20Deporte_2.jpg'
 			},
-			{
+            {
 				title: '',
-				description: 'Esta zona tenía lo que se conoce como un índice de compacidad alto: demasiado espacio construido en relación al poco espacio público disponible. Los puntos más  oscuros del mapa representan las zonas que cuentan con menos espacio público, y por lo tanto el índice de compacidad es alto.',
-				type: 'map',
-				url:'http://deluxeargentina.com/wp-content/themes/deluxe/timthumb.php?w=1000&h=450&src=http://deluxeargentina.com/wp-content/uploads/2012/05/Buenos-Aires-by-night.resized.jpg'
-			}
-			,
-			{
-				title: '',
-				description: 'Por eso transformamos el microcentro en un área de prioridad peatonal.',
-				type: 'map',
-				url:'http://i1.wp.com/www.buenasdicas.com/wp-content/uploads/2015/11/buenos-aires-capa.jpg'
-			}
-			,
-			{
-				title: '',
-				description: 'Miremos el mapa del indicador de compacidad corregida de nuevo: los puntos que vimos antes ya no son tan oscuros. Eso es porque hoy hay más espacio para caminar tranquilos entre tanto edificio, hay más espacio para disfrutar.',
-				type: 'map',
-				url:'http://argentinafreetravel.com/imgs/ban3.jpg'
+				description: 'En estas manzanas se encuentra el nuevo Barrio Parque Donado Holmberg, que potenciará el desarrollo social, cultural, económico y administrativo de la zona.',
+				type: 'image',
+                url:'http://www.buenosaires.gob.ar/sites/gcaba/files/styles/interna_pagina/public/3_231.jpg'
 			}
 		]
 	},
-	{
-		title: '',
-		order: 999,
+    {
+		title: 'Encuentro cercano con la Ciudad',
+		order: 5,
+		category: 'cercana',
+		slides: [
+			{
+				title: 'Encuentro cercano con la Ciudad',
+				description: 'Una ciudad es cercana cuando viajar de un extremo al otro es sencillo, accesible y sustentable.',
+				type: 'image', 
+                url:'https://media.giphy.com/media/MrmgBz2qYDtOE/giphy.gif'
+			},
+            {
+				title: '',
+				description: 'La construcción del Metrobús en las principales arterias de Buenos Aires contribuye a que todos cuenten con transporte transporte público cómodo y rápido para que vayas a donde quieras, cuando quieras, sin tardar más de lo necesario. Porque la Ciudad es tuya.',
+				type: 'map', 
+                url:'http://epok.buenosaires.gob.ar/pub/mapa/sociopublico/ciudad_cercana/'
+			}
+		]
+	},
+    {
+		title: 'Explorá',
+		order: 0,
 		category: false,
 		slides: [
 			{
 				title: 'Explorá',
-				description: 'Conocé todo lo que el Ministerio de Desarrollo Urbano y Transporte está haciendo para mejorar la Ciudad. Explorá todos los mapas. Explorá todas las obras. Conocé nuestra Visión Ciudad.',
-				type: 'image',
-				url:'http://www.heavenonearthvacation.com/a2.jpg'
-			}	
+				description: 'Conocé todo lo que el Ministerio de Desarrollo Urbano y Transporte está haciendo para mejorar la Ciudad. <a>Explorá todos los mapas</a>. Explorá todas las obras.',
+				type:'image', url:'http://cdn2.buenosaires.gob.ar/desarrollourbano/sociopublico/acceso_constitucion/comuna1constitucion.jpg'
+			}
 		]
-	},
+	}
 ];
